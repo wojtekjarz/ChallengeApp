1e631d5ced5f4d6ad467d1e993ef38ef65614f9c
wojtekjarz <mentis@poczta.fm>
2023-10-02 19:54:26 +0100

    zad dom 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.ComponentModel.Design;
+
+var imie = "Ewa";
+var plec = "kobieta";
+var wiek = 19;
+
+if (imie == "Ewa" && wiek == 33 && plec =="kobieta")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+else if (plec == "kobieta" && wiek < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+
+else if (plec == "mezczyzna" && wiek <18) 
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
